0e51e1883a5e75e883036574c976c830039ecd92
crevillo <crevillo@gmail.com>
2015-06-05 20:12:36 +0200

    First commit

---
+++ CrevilloEzSyliusBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 5/06/15
+ * Time: 23:01
+ */
+
+namespace Crevillo\EzSyliusBundle;
+
+use EzSystems\DemoBundle\EzSystemsDemoBundle;
+
+class CrevilloEzSyliusBundle extends EzSystemsDemoBundle
+{
+    protected $name = 'eZSyliusBundle';
+}
