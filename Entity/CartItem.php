4870441e85248dff21fe83fd9ca407cf6c5fee29
crevillo <crevillo@gmail.com>
2015-06-06 10:08:21 +0200

    CartItem entity and doctrine mapping def

---
+++ Entity/CartItem.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File containing the CartItem class.
+ *
+ * @copyright Copyright (C) crevillo@gmail.com. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version //autogentag//
+ */
+namespace Crevillo\EzSyliusBundle;
+
+use Sylius\Component\Cart\Model\CartItem as BaseCartItem;
+
+class CartItem extends BaseCartItem
+{
+
+}
