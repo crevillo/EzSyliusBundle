0cbf9d6985be0ba73a4d66cb2225225f128ef268
crevillo <crevillo@gmail.com>
2015-06-08 20:07:53 +0200

    removed not needed setting and action

--- Controller/CartController.php
+++ Controller/CartController.php
@@ -11,25 +11,5 @@ use eZ\Bundle\EzPublishCoreBundle\Controller;
 
 class CartController extends Controller
 {
-    /**
-     * Renders product in block item view
-     *
-     * @param $locationId
-     * @param $viewType
-     * @param bool $layout
-     * @param array $params
-     */
-    public function blockItemViewAction( $locationId, $viewType, $layout = false, array $params = array() )
-    {
-        $form = $this->createForm( $this->get( 'ezsylius.form.type.add_to_cart' ) );
 
-        return $this->get( 'ez_content' )->viewLocation(
-            $locationId,
-            $viewType,
-            $layout,
-            array(
-                'form' => $form->createView()
-            ) + $params
-        );
-    }
 }
