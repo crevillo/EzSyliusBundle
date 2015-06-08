05e24bc0bbee7fdc9d943a7290953ddd5e995d93
crevillo <crevillo@gmail.com>
2015-06-08 21:00:35 +0200

    added pricing dep and started summary template

--- Controller/CartController.php
+++ Controller/CartController.php
@@ -7,9 +7,30 @@
 
 namespace Crevillo\EzSyliusBundle\Controller;
 
-use eZ\Bundle\EzPublishCoreBundle\Controller;
+use \Sylius\Bundle\CartBundle\Controller\CartController as SyliusCartController;
 
-class CartController extends Controller
+class CartController extends SyliusCartController
 {
+    /**
+     * Displays current cart summary page.
+     * The parameters includes the form created from `sylius_cart` type.
+     *
+     * @return Response
+     */
+    public function summaryAction()
+    {
+        $cart = $this->getCurrentCart();
+        $form = $this->createForm('sylius_cart', $cart);
 
+        $view = $this
+            ->view()
+            ->setTemplate('eZSyliusBundle:cart:summary.html.twig')
+            ->setData(array(
+                'cart' => $cart,
+                'form' => $form->createView()
+            ))
+        ;
+
+        return $this->handleView($view);
+    }
 }
